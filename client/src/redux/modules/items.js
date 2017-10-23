ae43d7210800dacfafd3789a47f3c7fa8dfc16de
Kenneth Chow <kennethchow74@gmail.com>
2017-10-22 22:58:26 -0700

    edited items and users actions/reducers

--- client/src/redux/modules/items.js
+++ client/src/redux/modules/items.js
@@ -1,25 +1,62 @@
 import { mainURL } from '../constants'
 import { getUsers } from '../actions/userActions'
 
+/**
+ * NO NEED FOR ERRORMSG OR LOADING PROPERTIES AS APOLLO 
+ * CLIENT HAS THEIR OWN.
+ */
+// INITIAL STATE 
+const initialState = {
+    filteredItems: []
+}
 
+// ACTIONS
+const GET_CARD_ITEMS_BEGIN = 'GET_CARD_ITEMS_BEGIN'
+const GET_CARD_ITEMS_SUCCESS = 'GET_CARD_ITEMS_SUCCESS'
+const GET_CARD_ITEMS_ERROR = 'GET_CARD_ITEMS_ERROR'
 
 // ACTION CREATORS
-const getCardItemsBegin = () => {
+export const getCardItemsBegin = () => {
     return {
         type: 'GET_CARD_ITEMS_BEGIN'
     }
 }
-const getCardItemsSuccess = (items, users) => {
+export const getCardItemsSuccess = (items, users) => {
     return {
         type: 'GET_CARD_ITEMS_SUCCESS', 
         users,
         items
     }
 }
-const getCardItemsError = (error) => {
+export const getCardItemsError = (error) => {
     return {
         type: 'GET_CARD_ITEMS_ERROR', 
         error
+    }
+}
+
+
+// REDUCER
+export default ( state = initializeState, action ) => {
+    switch (action.type) {
+        case 'GET_CARD_ITEMS_BEGIN':
+            return {
+                ...state,
+                isLoading: true,
+            }
+        case 'GET_CARD_ITEMS_SUCCESS':
+            return {
+                ...state,
+                isLoading: false,
+            }
+        case 'GET_CARD_ITEMS_ERROR':
+            return {
+                ...state,
+                errorMsg: action.error,
+                isLoading: false
+            }
+        default:
+            return state
     }
 }
 
